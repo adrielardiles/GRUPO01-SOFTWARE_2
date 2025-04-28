8e31e21c7e3b3ed69b308a03a05158960d78f1f4
AdrielMarocho <adrielardiles132004@gmail.com>
2025-04-28 16:47:00 -0500

    frameworks creados

---
+++ Backend/roomieYA_backend/src/main/java/com/edu/roomieyabackend/RoomieYaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edu.roomieyabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomieYaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomieYaBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/roomieYA_backend/src/test/java/com/edu/roomieyabackend/RoomieYaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.roomieyabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomieYaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
