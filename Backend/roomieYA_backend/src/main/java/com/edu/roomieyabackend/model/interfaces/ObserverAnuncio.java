ba0968e03a7afc3183be7c802696fd69741d19d1
LeoPC2004 <lmpc995205@gmail.com>
2025-05-21 23:28:46 -0500

    FixAnuncio

--- Backend/roomieYA_backend/src/main/java/com/edu/roomieyabackend/model/interfaces/ObserverAnuncio.java
+++ Backend/roomieYA_backend/src/main/java/com/edu/roomieyabackend/model/interfaces/ObserverAnuncio.java
@@ -1,5 +1,7 @@
 package com.edu.roomieyabackend.model.interfaces;
 
+import com.edu.roomieyabackend.model.entities.Anuncio;
+
 public interface ObserverAnuncio {
     void actualizar(Anuncio anuncio);
 }
